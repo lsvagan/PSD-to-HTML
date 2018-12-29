fffca9c5743085161329cf8ff08aa8705aabeb64
lsvagan <luka.svagan@gmail.com>
2018-12-29 21:30:34 +0100

    creating a banner slider and adding multiple items in trending items section

--- main.js
+++ main.js
@@ -22,4 +22,68 @@ $(document).ready(function(){
   })
 
 
-});
+  $('.subscribe').on('mouseenter', function () {
+     $(this).find('.hover1').addClass('hover');
+  })
+
+  $('.subscribe').mouseleave(function () {
+     $(this).find('.hover1').removeClass('hover');
+  })
+
+  /*banner slider*/
+
+  let $bannertext = $('.bannertext ul li');
+  let $bannerdots = $('.bannerdot');
+  let index = -1;
+  
+  showText();
+
+  function showText() {
+     index++;
+     if(index > $bannertext.length - 1){
+        index = 0;
+     }
+     $($bannertext).css({'display': 'none'});
+     $($bannertext[index]).css({'display': 'inline-block'});
+     $($bannerdots).removeClass('banneractive');
+     $($bannerdots[index]).addClass('banneractive');
+
+     setTimeout(showText,5000)
+  }
+
+  $($bannerdots).on('click', function(){
+   index = $(this).index()
+   changeText(index); 
+ })
+
+ function changeText (n) {
+   $($bannertext).css({'display': 'none'});
+   $($bannertext[n]).css({'display': 'inline-block'});
+   $($bannerdots).removeClass('banneractive');
+   $($bannerdots[n]).addClass('banneractive');
+ }
+
+/*trending items slider */
+
+  let $items = $('.trendingitems ul li');
+  let $dots = $('.dot');
+  let n = 0;
+  
+  showItem(n);
+
+  function showItem(n) {
+     
+     $($items).css({'display': 'none'});
+     $($items[n]).css({'display': 'inline-block'});
+     $($dots).removeClass('active');
+     $($dots[n]).addClass('active');
+
+  }
+
+  $($dots).on('click', function(){
+   n = $(this).index()
+   showItem(n); 
+ })
+
+
+})
